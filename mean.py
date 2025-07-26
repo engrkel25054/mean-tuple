2e3574534585e5a9f4e4f4cd15ac700b5d5a107e
Brightsparks_26 <104679047+engrkel25054@users.noreply.github.com>
2025-07-26 20:26:25 +0400

    Update mean.py

--- mean.py
+++ mean.py
@@ -2,7 +2,7 @@
 """
 Write an expression that evaluates to the mean of a tuple of numbers. Use the function sum.
 """
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 mean = sum(numbers) / len(numbers)
 print("mean:", mean)
